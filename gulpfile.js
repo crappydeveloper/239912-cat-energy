ba84e0c0c827973807167d7e1e3c99921b857e73
Artem Lapkin <30687070+crappydeveloper@users.noreply.github.com>
2018-02-15 22:19:04 +0300

    Еще что-то наделал в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,26 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require('gulp-svgstore');
+var run = require('run-sequence');
+var del = require("del");
 var server = require("browser-sync").create();
 
-gulp.task("style", function() {
+gulp.task("style", () => {
   gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("images", function() {
-   gulp.src("source/img/**/*.{png,jpg,svg}")
+gulp.task("images", () => {
+  gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
@@ -36,24 +38,43 @@ gulp.task("images", function() {
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("webp", function() {
-   gulp.src("source/img/**/*.{png,jpg}")
+gulp.task("webp", () => {
+  gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("sprite", function() {
-   gulp.src("source/img/icon-*.svg")
+gulp.task("sprite", () => {
+  gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("build", (done) => {
+  run("clean", "copy", "style", "sprite", done)
+});
+
+gulp.task("copy", () => {
+  gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", () => {
+  return del("build");
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
