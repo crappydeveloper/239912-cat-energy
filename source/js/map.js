7ff96e40f985c2748141628f8c7a8241af8f0d24
Artem Lapkin <artemka666@mail.ru>
2018-02-22 19:52:23 +0300

    Пожалуй, последний коммит

--- source/js/map.js
+++ source/js/map.js
@@ -7,6 +7,11 @@ function initMap() {
     };
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  var marker = new google.maps.Marker({
+          position: centerLatLng,
+          map: map
+        });
 }
 
 google.maps.event.addDomListener(window, "load", initMap);
